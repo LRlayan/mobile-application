ec944140b1043019670056794897987ea3bdc886
LRLayan <rameshlayan4@gmail.com>
2025-02-22 02:32:37 +0530

    search bar and animated FAB

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,30 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { AnimatedFAB, Searchbar } from "react-native-paper";
 
 export default function Tab() {
+    const [searchQuery, setSearchQuery] = React.useState('');
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Countdown</Text>
+            <Text style={styles.title}>Countdowns</Text>
+
+            <Searchbar
+                placeholder="Search"
+                onChangeText={setSearchQuery}
+                value={searchQuery}
+            />
+
+            <AnimatedFAB
+                icon="plus"
+                label=""
+                extended={false}
+                onPress={() => console.log("FAB Pressed")}
+                animateFrom="right"
+                iconMode="static"
+                color='white'
+                style={styles.fab}
+            />
         </View>
     );
 }
@@ -11,11 +32,19 @@ export default function Tab() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 20
+        padding: 20,
     },
     title: {
         fontSize: 35,
-        color: 'purple',
-        fontWeight: "bold"
-    }
+        color: "purple",
+        fontWeight: "bold",
+        marginBottom: 20
+    },
+    fab: {
+        position: "absolute",
+        right: 20,
+        bottom: 20,
+        backgroundColor: "#d990f0",
+        color: 'white'
+    },
 });
