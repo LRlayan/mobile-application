0e575bda7d5b3e510ba72c98db4f066e74f59534
LRLayan <rameshlayan4@gmail.com>
2025-02-23 03:08:11 +0530

    change data type

--- app/model/countdown-model.ts
+++ app/model/countdown-model.ts
@@ -1,12 +1,15 @@
 export class CountdownModel{
     title: string;
-    date: string;
-    time: string;
+    date: Date;
+    time: { hours: number; minutes: number } | undefined;
     repeat: string;
     color: string;
     notes: string;
 
-    constructor(title: string, date: string, time: string, repeat: string, color: string, notes: string) {
+    constructor(title: string, date: Date, time: {
+        hours: number;
+        minutes: number
+    } | undefined, repeat: string, color: string, notes: string) {
         this.title = title;
         this.date = date;
         this.time = time;
