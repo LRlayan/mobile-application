0c4b029338e24b510b1829383fbd86dcb9164b54
LRLayan <rameshlayan4@gmail.com>
2025-02-23 03:07:51 +0530

    card

---
+++ app/component/card.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { Card, Text } from 'react-native-paper';
+import { CountdownModel } from '../model/countdown-model';
+import {StyleSheet} from "react-native";
+
+interface CountdownCardProps {
+    data: CountdownModel[];
+}
+
+const CountdownCard: React.FC<CountdownCardProps> = ({ data }) => {
+    return (
+        <>
+            {data.map((item: CountdownModel, index: number) => (
+                <Card key={index} style={{ marginBottom: 10 }}>
+                    <Card.Content style={styles.container}>
+                        <Text variant="titleLarge">{item.title}</Text>
+                        <Text variant="bodyMedium">{item.repeat}</Text>
+                        <Text variant="bodyMedium">{item.color}</Text>
+                        <Text variant="bodyMedium">{item.time ? `${item.time.hours}:${item.time.minutes}` : 'No time set'}</Text>
+                        <Text variant="bodyMedium">{item.date ? `${item.date.toDateString()}` : new Date().toDateString()}</Text>
+                        <Text variant="bodyMedium">{item.notes}</Text>
+                    </Card.Content>
+                    {/*<Card.Cover source={{ uri: 'https://picsum.photos/700' }} />*/}
+                    {/*<Card.Actions>*/}
+                    {/*    <Button>Cancel</Button>*/}
+                    {/*    <Button>Ok</Button>*/}
+                    {/*</Card.Actions>*/}
+                </Card>
+            ))}
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+    },
+});
+
+export default CountdownCard;
