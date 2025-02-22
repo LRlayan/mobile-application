85116d4482b4725f336ff3ec8b60befd30ae6226
LRLayan <rameshlayan4@gmail.com>
2025-02-23 03:07:39 +0530

    wrap Stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,18 @@
 import { Stack } from 'expo-router/stack';
 import { PaperProvider } from "react-native-paper";
 import React from "react";
+import {Provider} from "react-redux";
+import {store} from "./store/store";
 
 export default function Layout() {
     return (
-        <PaperProvider>
-            <Stack>
-                <Stack.Screen name="index" options={{headerShown: false}}/>
-                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            </Stack>
-        </PaperProvider>
+        <Provider store={store}>
+            <PaperProvider>
+                <Stack>
+                    <Stack.Screen name="index" options={{headerShown: false}}/>
+                    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                </Stack>
+            </PaperProvider>
+        </Provider>
     );
 }
