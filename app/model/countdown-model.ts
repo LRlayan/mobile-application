21c915cb0adb863d796ed98da653fdfc650fa78c
LRLayan <rameshlayan4@gmail.com>
2025-02-25 20:53:01 +0530

    selectedUnits

--- app/model/countdown-model.ts
+++ app/model/countdown-model.ts
@@ -5,16 +5,18 @@ export class CountdownModel{
     repeat: string;
     color: string;
     notes: string;
+    selectedUnits: string[]
 
     constructor(title: string, date: Date, time: {
         hours: number;
         minutes: number
-    } | undefined, repeat: string, color: string, notes: string) {
+    } | undefined, repeat: string, color: string, notes: string, selectedUnits: string[]) {
         this.title = title;
         this.date = date;
         this.time = time;
         this.repeat = repeat;
         this.color = color;
         this.notes = notes;
+        this.selectedUnits = selectedUnits;
     }
 }
