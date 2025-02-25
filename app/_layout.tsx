3edd6a624f7c67e61affcf54aa5fcc088bccfbfa
LRLayan <rameshlayan4@gmail.com>
2025-02-25 20:13:26 +0530

    ignoreLogs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,14 @@ import { PaperProvider } from "react-native-paper";
 import React from "react";
 import {Provider} from "react-redux";
 import {store} from "./store/store";
+import { LogBox } from "react-native";
 
 export default function Layout() {
+
+    LogBox.ignoreLogs([
+        "VirtualizedLists should never be nested inside plain ScrollViews",
+    ]);
+
     return (
         <Provider store={store}>
             <PaperProvider>
