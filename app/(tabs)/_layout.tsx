010b03e21e95db52e75ef2f9c4d620ccfce7ee98
LRLayan <rameshlayan4@gmail.com>
2025-02-26 15:17:46 +0530

    replace bottom navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,57 +1,96 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { Tabs } from "expo-router";
-import { Text, View, StyleSheet, ViewStyle, TextStyle } from "react-native";
+import React from 'react';
+import {StyleSheet, ViewStyle, TextStyle} from 'react-native';
+
+import { CommonActions } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { BottomNavigation } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {SettingsScreen} from "./settings";
+import HomeScreen from "./index";
+import {MoreScreen} from "./more";
+
+const Tab = createBottomTabNavigator();
 
 export default function TabLayout() {
     return (
-        <Tabs
+        <Tab.Navigator
             screenOptions={{
-                tabBarActiveTintColor: "purple",
-                tabBarStyle: { height: 60 },
+                headerShown: false,
             }}
+            tabBar={({ navigation, state, descriptors, insets }) => (
+                <BottomNavigation.Bar
+                    navigationState={state}
+                    safeAreaInsets={insets}
+                    onTabPress={({ route, preventDefault }) => {
+                        const event = navigation.emit({
+                            type: 'tabPress',
+                            target: route.key,
+                            canPreventDefault: true,
+                        });
+
+                        if (event.defaultPrevented) {
+                            preventDefault();
+                        } else {
+                            navigation.dispatch({
+                                ...CommonActions.navigate(route.name, route.params),
+                                target: state.key,
+                            });
+                        }
+                    }}
+                    renderIcon={({ route, focused, color }) => {
+                        const { options } = descriptors[route.key];
+                        if (options.tabBarIcon) {
+                            return options.tabBarIcon({ focused, color, size: 24 });
+                        }
+
+                        return null;
+                    }}
+                    getLabelText={({ route }) => {
+                        const { options } = descriptors[route.key];
+                        const label =
+                            typeof options.tabBarLabel === "string"
+                                ? options.tabBarLabel
+                                : typeof options.title === "string"
+                                    ? options.title
+                                    : route.name; // Use route.name as a fallback
+
+                        return label;
+                    }}
+                />
+            )}
         >
-            <Tabs.Screen
-                name="index"
+            <Tab.Screen
+                name="Home"
+                component={HomeScreen}
                 options={{
-                    title: "Home",
-                    tabBarIcon: ({ color }) => (
-                        <View style={styles.iconContainer as ViewStyle}>
-                            <FontAwesome size={28} name="home" color={color} />
-                            <Text style={styles.label as TextStyle}>Home</Text>
-                        </View>
-                    ),
-                    tabBarLabel: () => null, // Hide default label
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({ color, size }) => {
+                        return <Icon name="home" size={size} color={color} />;
+                    },
                 }}
             />
-
-            <Tabs.Screen
-                name="settings"
+            <Tab.Screen
+                name="Settings"
+                component={SettingsScreen}
                 options={{
-                    title: "Settings",
-                    tabBarIcon: ({ color }) => (
-                        <View style={styles.iconContainer as ViewStyle}>
-                            <FontAwesome size={28} name="cog" color={color} />
-                            <Text style={styles.label as TextStyle}>Setting</Text>
-                        </View>
-                    ),
-                    tabBarLabel: () => null, // Hide default label
+                    tabBarLabel: 'Settings',
+                    tabBarIcon: ({ color, size }) => {
+                        return <Icon name="cog" size={size} color={color} />;
+                    },
                 }}
             />
-
-            <Tabs.Screen
-                name="more"
+            <Tab.Screen
+                name="More"
+                component={MoreScreen}
                 options={{
-                    title: "More",
-                    tabBarIcon: ({ color }) => (
-                        <View style={styles.iconContainer as ViewStyle}>
-                            <FontAwesome size={28} name="bars" color={color} />
-                            <Text style={styles.label as TextStyle}>More</Text>
-                        </View>
-                    ),
-                    tabBarLabel: () => null, // Hide default label
+                    tabBarLabel: 'More',
+                    tabBarIcon: ({ color, size }) => {
+                        return <Icon name="dots-horizontal" size={size} color={color} />;
+                    },
                 }}
             />
-        </Tabs>
+        </Tab.Navigator>
     );
 }
 
@@ -66,5 +105,10 @@ const styles = StyleSheet.create({
         color: "black",
         minWidth: 70,
         flexWrap: "nowrap" as TextStyle["flexWrap"]
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
 });
