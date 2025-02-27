704e5249dfa2b824944a035e178864df784b92c0
LRLayan <rameshlayan4@gmail.com>
2025-02-27 14:02:43 +0530

    getAllCardsService

--- service/countdowns-service.ts
+++ service/countdowns-service.ts
@@ -1,5 +1,5 @@
 import {CountdownsModel} from "../model/countdowns-model";
-import {saveCard} from "../repository/countdowns-repository";
+import {deleteCard, getAllCards, saveCard, updateCard} from "../repository/countdowns-repository";
 
 export async function saveCardService(card: CountdownsModel) {
     try {
@@ -7,5 +7,14 @@ export async function saveCardService(card: CountdownsModel) {
     } catch (e) {
         console.error("Service layer error: Failed to save cards!", e);
         throw new Error("Failed to save cards. Please try again.");
+    }
+}
+
+export async function getAllCardsService() {
+    try {
+         return await getAllCards();
+    } catch (e) {
+        console.log("Failed to get all card!", e);
+        throw e;
     }
 }
