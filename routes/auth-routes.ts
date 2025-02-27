8503958669f33c99f717e72c6d1774278ac0f197
LRLayan <rameshlayan4@gmail.com>
2025-02-27 15:59:18 +0530

    refresh token

--- routes/auth-routes.ts
+++ routes/auth-routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from 'dotenv';
+import jwt, {Secret} from 'jsonwebtoken';
 
 dotenv.config();
 
@@ -24,8 +25,15 @@ router.post("/register", async (req, res) => {
 });
 
 router.post("/refresh-token", async (req, res) => {
-    try{
+    const authHeader = req.headers.authorization;
+    const refresh_token = authHeader?.split(' ')[1];
 
+    if(!refresh_token)res.status(401).send('No token provided');
+
+    try{
+        const payload = jwt.verify(refresh_token as string, process.env.REFRESH_TOKEN as Secret) as {username: string, iat: number};
+        const token = jwt.sign({ username: payload.username }, process.env.SECRET_KEY as Secret, {expiresIn: "7d"});
+        res.json({accessToken : token});
     }catch(err){
         console.log(err);
         res.status(401).json(err);
