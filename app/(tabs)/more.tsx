197991d03b2974c3b91a5e318f1a054575bfbf51
LRLayan <rameshlayan4@gmail.com>
2025-02-28 03:13:21 +0530

    more screen

--- app/(tabs)/more.tsx
+++ app/(tabs)/more.tsx
@@ -1,19 +1,71 @@
 import React from "react";
-import { View, StyleSheet } from 'react-native';
-import { Text } from 'react-native-paper';
+import { View, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
+import { Text, Divider, List } from "react-native-paper";
+
+interface OptionItemProps {
+    title: string;
+}
 
 export function MoreScreen() {
     return (
         <View style={styles.container}>
-            <Text variant="headlineMedium">More!</Text>
+            <Text variant="headlineMedium" style={styles.title}>More</Text>
+
+            <ScrollView style={styles.scrollContainer}>
+                <Divider/>
+
+                <List.Section>
+                    <OptionItem title="Version" />
+                    <OptionItem title="Developer" />
+                </List.Section>
+
+                <Divider />
+
+                <List.Section>
+                    <OptionItem title="Write a Review" />
+                    <OptionItem title="Share App" />
+                </List.Section>
+
+                <Divider />
+
+                <List.Section>
+                    <OptionItem title="Privacy Policy & Terms of Use" />
+                    <OptionItem title="Help Center" />
+                    <OptionItem title="Contact Us" />
+                </List.Section>
+            </ScrollView>
         </View>
     );
 }
 
+const OptionItem: React.FC<OptionItemProps> = ({ title }) => (
+    <TouchableOpacity style={styles.option}>
+        <List.Item title={title} titleStyle={styles.optionText} />
+    </TouchableOpacity>
+);
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        backgroundColor: "#fff",
+        paddingTop: 69,
+    },
+    title: {
+        fontSize: 30,
+        textAlign: "left",
+        marginLeft: 20,
+        marginBottom: 20,
+    },
+    scrollContainer: {
+        flex: 1,
+    },
+    option: {
+        paddingVertical: 0,
+        paddingHorizontal: 10,
+    },
+    optionText: {
+        fontSize: 16,
     },
 });
+
+export default MoreScreen;
