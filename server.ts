2cef8e6009a8874b308fbbfd628345cf54edd77b
LRLayan <rameshlayan4@gmail.com>
2025-02-27 16:56:31 +0530

    authenticate token and auth route

--- server.ts
+++ server.ts
@@ -1,17 +1,20 @@
 import express from 'express';
 import {CountdownsRoutes} from "./routes/countdowns-routes";
-const cors = require('cors');
+import authRoutes from "./routes/auth-routes";
+import cors from 'cors';
+import {authenticateToken} from "./middleware/authenticate";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 app.use(cors({
     origin: 'http://localhost:8081',
-    method: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
 }));
 
-app.use('/api/v1/countdown',CountdownsRoutes);
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/countdown',authenticateToken,CountdownsRoutes);
 
 app.listen(3002, () => console.log("Server start 3002 port"));
