00760a71791d79b0d03d6355fe3520d1491531cc
LRLayan <rameshlayan4@gmail.com>
2025-02-27 16:18:29 +0530

    saveUser

--- repository/user-repository.ts
+++ repository/user-repository.ts
@@ -1,8 +1,19 @@
 import bcrypt from "bcrypt";
+import {PrismaClient} from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export async function saveUser(user: { username: string; email: string; password: string }) {
     try {
-
+        const hashedPassword = await bcrypt.hash(user.password, 10)
+        const newUser = await prisma.user.create({
+            data: {
+                username: user.username,
+                email: user.email,
+                password: hashedPassword
+            }
+        });
+        return newUser;
     } catch (error) {
         console.error("Error saving user:", error);
         throw error;
